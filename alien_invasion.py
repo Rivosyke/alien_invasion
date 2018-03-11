3c3a2bdb99457d83e69940b135a05b07c4688a5c
rivosyke <rcmcginn82@gmail.com>
2018-03-11 11:18:22 +0100

    11-MAR-2018

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,20 +1,33 @@
 #!/usr/bin/python
 
 import sys
-
 import pygame
+from Settings import Settings
+from ship import Ship
 
 def run_game():
-	pygame.init()
-	screen = pygame.display.set_mode((1200,800))
+	# Initialize pygame, settings, and screen object
+	pygame.display.init()
+	pygame.mixer.quit()
+	ai_settings = Settings()
+	screen = pygame.display.set_mode(
+		(ai_settings.screen_width, ai_settings.screen_width))
 	pygame.display.set_caption("Alien Invasion")
+	bg_color = (230,230,230)
+	
+	ship = Ship(screen)
 	
 	while True:
 		
+		# Watch for keyboard and mouse events
 		for event in pygame.event.get():
-			if event.type == pygame.QUIT():
+			if event.type == pygame.QUIT:
 				sys.exit()
 		
+		screen.fill(ai_settings.bg_color)
+		ship.blitme()
+		
+		# Make the most recently drawn screen visible
 		pygame.display.flip()
 		
 
