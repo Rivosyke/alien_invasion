e4232a010436415cc7f0c1c8dfa9679873c541b6
Rivosyke <rcmcginn82@gmail.com>
2018-03-21 15:40:15 +0100

    21-MAR-2018

--- Settings.py
+++ Settings.py
@@ -12,9 +12,10 @@ class Settings():
 		self.ship_speed_factor = 2
 		
 		# Bullet Settings
-		self.bullet_speed_factor = 1
+		self.bullet_speed_factor = 2
 		self.bullet_width = 3
-		self.bullet_height = 15
+		self.bullet_height = 8
 		self.bullet_color = 60,60,60
+		self.bullets_allowed = 4
 		
 		
--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import pygame
+from pygame.sprite import Group
 from Settings import Settings
 from ship import Ship
 import game_functions as gf
@@ -16,12 +17,18 @@ def run_game():
 	bg_color = ai_settings.bg_color
 	
 	ship = Ship(ai_settings, screen)
+	# Group for bullets
+	bullets = Group()
 	
 	while True:		
 		# Watch for keyboard and mouse events
-		gf.check_events(ship)	
+		gf.check_events(ai_settings, screen, ship, bullets)	
 		ship.update()	
-		gf.update_screen(ai_settings, screen, ship)
+		bullets.update()		
+		gf.update_bullets(bullets)		
+		gf.update_screen(ai_settings, screen, ship, bullets)
+		
+		
 		
 		
 run_game()
